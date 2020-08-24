0c605445a7bfe3f3a18642d43302c067828527b0
bobwzb <42678607+bobwzb@users.noreply.github.com>
2020-08-24 01:44:44 -0700

    finish the device part function

--- EasyC++Render/EasyC++Render.cpp
+++ EasyC++Render/EasyC++Render.cpp
@@ -440,6 +440,142 @@ typedef struct {
 	IUINT32 background;        
 	IUINT32 foreground;         
 }	device;
+#define RENDER_STATE_WIREFRAME      1		
+#define RENDER_STATE_TEXTURE        2		
+#define RENDER_STATE_COLOR          4
+//initialize the device
+void device_init(device &device, int width, int height, void *fb) {
+	int need = sizeof(void*) * (height * 2 + 1024) + width * height * 8;
+	char *ptr = (char*)malloc(need + 64);
+	char *framebuf, *zbuf;
+	int j;
+	assert(ptr);
+	device.framebuffer = (IUINT32**)ptr;
+	device.zbuffer = (float**)(ptr + sizeof(void*) * height);
+	ptr += sizeof(void*) * height * 2;
+	device.texture = (IUINT32**)ptr;
+	ptr += sizeof(void*) * 1024;
+	framebuf = (char*)ptr;
+	zbuf = (char*)ptr + width * height * 4;
+	ptr += width * height * 8;
+	if (fb != NULL) framebuf = (char*)fb;
+	for (j = 0; j < height; j++) {
+		device.framebuffer[j] = (IUINT32*)(framebuf + width * 4 * j);
+		device.zbuffer[j] = (float*)(zbuf + width * 4 * j);
+	}
+	device.texture[0] = (IUINT32*)ptr;
+	device.texture[1] = (IUINT32*)(ptr + 16);
+	memset(device.texture[0], 0, 64);
+	device.tex_width = 2;
+	device.tex_height = 2;
+	device.max_u = 1.0f;
+	device.max_v = 1.0f;
+	device.width = width;
+	device.height = height;
+	device.background = 0xc0c0c0;
+	device.foreground = 0;
+	device.transform=transform_init(width, height);
+	device.render_state = RENDER_STATE_WIREFRAME;
+}
+// delete device
+void device_destroy(device device) {
+	if (device.framebuffer)
+		free(device.framebuffer);
+	device.framebuffer = NULL;
+	device.zbuffer = NULL;
+	device.texture = NULL;
+}
+// set the texture
+void device_set_texture(device& device, void *bits, long pitch, int w, int h) {
+	char *ptr = (char*)bits;
+	int j;
+	assert(w <= 1024 && h <= 1024);
+	for (j = 0; j < h; ptr += pitch, j++) 	// 重新计算每行纹理的指针
+		device.texture[j] = (IUINT32*)ptr;
+	device.tex_width = w;
+	device.tex_height = h;
+	device.max_u = (float)(w - 1);
+	device.max_v = (float)(h - 1);
+}
+// draw point
+void device_pixel(device& device, int x, int y, IUINT32 color) {
+	if (((IUINT32)x) < (IUINT32)device.width && ((IUINT32)y) < (IUINT32)device.height) {
+		device.framebuffer[y][x] = color;
+	}
+}
+// clear framebuffer and zbuffer
+void device_clear(device& device, int mode) {
+	int y, x, height = device.height;
+	for (y = 0; y < device.height; y++) {
+		IUINT32 *dst = device.framebuffer[y];
+		IUINT32 cc = (height - 1 - y) * 230 / (height - 1);
+		cc = (cc << 16) | (cc << 8) | cc;
+		if (mode == 0) cc = device.background;
+		for (x = device.width; x > 0; dst++, x--) dst[0] = cc;
+	}
+	for (y = 0; y < device.height; y++) {
+		float *dst = device.zbuffer[y];
+		for (x = device.width; x > 0; dst++, x--) dst[0] = 0.0f;
+	}
+}
+// draw the line
+void device_draw_line(device& device, int x1, int y1, int x2, int y2, IUINT32 c) {
+	int x, y, rem = 0;
+	if (x1 == x2 && y1 == y2) {
+		device_pixel(device, x1, y1, c);
+	}
+	else if (x1 == x2) {
+		int inc = (y1 <= y2) ? 1 : -1;
+		for (y = y1; y != y2; y += inc) device_pixel(device, x1, y, c);
+		device_pixel(device, x2, y2, c);
+	}
+	else if (y1 == y2) {
+		int inc = (x1 <= x2) ? 1 : -1;
+		for (x = x1; x != x2; x += inc) device_pixel(device, x, y1, c);
+		device_pixel(device, x2, y2, c);
+	}
+	else {
+		int dx = (x1 < x2) ? x2 - x1 : x1 - x2;
+		int dy = (y1 < y2) ? y2 - y1 : y1 - y2;
+		if (dx >= dy) {
+			if (x2 < x1) x = x1, y = y1, x1 = x2, y1 = y2, x2 = x, y2 = y;
+			for (x = x1, y = y1; x <= x2; x++) {
+				device_pixel(device, x, y, c);
+				rem += dy;
+				if (rem >= dx) {
+					rem -= dx;
+					y += (y2 >= y1) ? 1 : -1;
+					device_pixel(device, x, y, c);
+				}
+			}
+			device_pixel(device, x2, y2, c);
+		}
+		else {
+			if (y2 < y1) x = x1, y = y1, x1 = x2, y1 = y2, x2 = x, y2 = y;
+			for (x = x1, y = y1; y <= y2; y++) {
+				device_pixel(device, x, y, c);
+				rem += dx;
+				if (rem >= dy) {
+					rem -= dy;
+					x += (x2 >= x1) ? 1 : -1;
+					device_pixel(device, x, y, c);
+				}
+			}
+			device_pixel(device, x2, y2, c);
+		}
+	}
+}
+//read texture by position
+IUINT32 device_texture_read(device device, float u, float v) {
+	int x, y;
+	u = u * device.max_u;
+	v = v * device.max_v;
+	x = (int)(u + 0.5f);
+	y = (int)(v + 0.5f);
+	x = CMID(x, 0, device.tex_width - 1);
+	y = CMID(y, 0, device.tex_height - 1);
+	return device.texture[y][x];
+}
 int main()
 {
     std::cout << "Hello World!\n"; 
